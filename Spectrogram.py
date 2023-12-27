186caf3461c6274e3bad01aa67f4ab9df13d51d9
yahelil <117468179+yahelil@users.noreply.github.com>
2023-12-27 20:10:08 +0200

    Add files via upload

--- Spectrogram.py
+++ Spectrogram.py
@@ -1,8 +1,8 @@
+import cv2 as cv
 import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import  Image
 
 FRAME_SIZE = 2048
 HOP_SIZE = 100
@@ -10,7 +10,7 @@ HOP_SIZE = 100
 
 class Spec:
     def __init__(self):
-        self.file = "audio/scale.wav"
+        self.file = "audio/elior.wav"
         self.S_db = None
         self.BinerySpec = []
 
@@ -22,22 +22,44 @@ class Spec:
         Y_scale = np.abs(S_scale) ** 2
         self.S_db = librosa.amplitude_to_db(Y_scale, ref=np.max)
         for line in self.S_db:
-            self.BinerySpec.append([0 if x == -80.0 else 1 for x in line])
-
+            new_line = []
+            for x in line:
+                if -80.0 <= x <= -70.0:
+                    new_line.append(0)
+                elif -70.0 < x <= -60.0:
+                    new_line.append(0.15)
+                elif -60.0 < x <= -50.0:
+                    new_line.append(0.3)
+                elif -50.0 < x <= -40.0:
+                    new_line.append(0.45)
+                elif -40.0 < x <= -30.0:
+                    new_line.append(0.6)
+                elif -30.0 < x <= -20.0:
+                    new_line.append(0.75)
+                elif -20.0 < x <= -10.0:
+                    new_line.append(0.9)
+                else:
+                    new_line.append(1)
+            self.BinerySpec.append(new_line)
         plt.imshow(self.BinerySpec, cmap=plt.get_cmap('gray'))
+        plt.axis('off')
+        plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
+        plt.show()
+        img = cv.imread('original_spectrogram.png')
+        blur = cv.blur(img, (5, 5))
+        plt.subplot(121), plt.imshow(img), plt.title('Original')
+        plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(blur), plt.title('Blurred')
+        plt.xticks([]), plt.yticks([])
         plt.show()
 
-        # self.plot_spectrogram()
-
-    def plot_spectrogram(self):
-        fig, ax = plt.subplots()
-        img = librosa.display.specshow(self.S_db, x_axis='time', y_axis='linear', ax=ax)
-        ax.set(title='A Spectrogram of Hz on time')
-        fig.colorbar(img, ax=ax, format="%+2.f dB")
-        plt.show()
+    @staticmethod
+    def translate(value, src, dest):
+        srcSpan = src[1] - src[0]
+        destSpan = dest[1] - dest[0]
+        valueScaled = float(value - src[0]) / float(srcSpan)
+        return dest[0] + (valueScaled * destSpan)
 
 
 if __name__ == "__main__":
-     g = Spec()
-     g.scale()
-
+    Spec().scale()
---
+++ tmpspec.py
@@ -0,0 +1,42 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import  Image
+
+FRAME_SIZE = 2048
+HOP_SIZE = 100
+
+
+class Spec:
+    def __init__(self):
+        self.file = "audio/scale.wav"
+        self.S_db = None
+        self.BinerySpec = []
+
+    def scale(self):
+        scale, sr = librosa.load(self.file)  # returns audio time series, sampling rate of scale
+        S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+
+        # Creating a spectrogram out of the audio
+        Y_scale = np.abs(S_scale) ** 2
+        self.S_db = librosa.amplitude_to_db(Y_scale, ref=np.max)
+        for line in self.S_db:
+            self.BinerySpec.append([0 if x == -80.0 else 1 for x in line])
+
+        plt.imshow(self.BinerySpec, cmap=plt.get_cmap('gray'))
+        plt.show()
+
+        # self.plot_spectrogram()
+
+    def plot_spectrogram(self):
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(self.S_db, x_axis='time', y_axis='linear', ax=ax)
+        ax.set(title='A Spectrogram of Hz on time')
+        fig.colorbar(img, ax=ax, format="%+2.f dB")
+        plt.show()
+
+
+if __name__ == "__main__":
+     g = Spec()
+     g.scale()
