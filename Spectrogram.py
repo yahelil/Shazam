1e92f090885a841c7bdfd1762dbb533401d581c7
yahelil <117468179+yahelil@users.noreply.github.com>
2024-01-18 11:32:37 +0200

    Add files via upload

--- Spectrogram.py
+++ Spectrogram.py
@@ -1,4 +1,3 @@
-import cv2 as cv
 import librosa
 import librosa.display
 import numpy as np
@@ -10,9 +9,9 @@ HOP_SIZE = 100
 
 class Spec:
     def __init__(self):
-        self.file = "audio/elior.wav"
+        self.file = "audio/eyal4.wav"
         self.S_db = None
-        self.BinarySpec = []
+        self.PeaksSpec = []
 
     def scale(self):
         scale, sr = librosa.load(self.file)  # returns audio time series, sampling rate of scale
@@ -22,20 +21,35 @@ class Spec:
         Y_scale = np.abs(S_scale) ** 2
         self.S_db = librosa.amplitude_to_db(Y_scale, ref=np.max)
         Max, Min = self.max_spec(self.S_db)
+        old_range = [Min, Max]
+        new_range = [0, 1]
         for line in self.S_db:
-            old_range = [Min, Max]
-            new_range = [0, 1]
-            self.BinarySpec.append([self.translate(x, old_range, new_range)for x in line])
-        plt.imshow(self.BinarySpec, cmap=plt.get_cmap('gray'))
-        plt.axis('off')
+            self.PeaksSpec.append([self.translate(x, old_range, new_range)for x in line])
+        peaks = self.peaks(self.PeaksSpec)
+        rows, cols = len(self.PeaksSpec), len(self.PeaksSpec[0])
+        x = []
+        y = []
+        for row in range(rows):
+            if all(num == 0 for num in self.PeaksSpec[row]):
+                stop = -1 * (len(self.PeaksSpec) - row)
+                self.PeaksSpec = self.PeaksSpec[:stop]
+                break
+            for col in range(cols):
+                if self.PeaksSpec[row][col] > peaks[-1]:
+                    self.PeaksSpec[row][col] = 0.5
+                    if row > 3:
+                        x.append(col)
+                        y.append(row)
+                else:
+                    self.PeaksSpec[row][col] = 0.0
+        # Just the spectrogram
+        plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
         plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
         plt.show()
-        img = cv.imread('original_spectrogram.png')
-        blur = cv.blur(img, (5, 5))
-        plt.subplot(121), plt.imshow(img), plt.title('Original')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(122), plt.imshow(blur), plt.title('Blurred')
-        plt.xticks([]), plt.yticks([])
+        # The spectrogram with peaks
+        plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
+        plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
+        plt.scatter(x, y, color="red", s=3)
         plt.show()
 
     @staticmethod
@@ -48,7 +62,6 @@ class Spec:
             for j in range(col):
                 mymax = max(mymax, lst[i][j])
                 mymin = min(mymin, lst[i][j])
-        print(mymax, mymin)
         return mymax, mymin
 
     @staticmethod
@@ -57,6 +70,14 @@ class Spec:
         destSpan = dest[1] - dest[0]
         valueScaled = float(value - src[0]) / float(srcSpan)
         return dest[0] + (valueScaled * destSpan)
+
+    @staticmethod
+    def peaks(lst):
+        arr = np.array(lst)
+        origin = arr
+        partition = np.argpartition(origin.ravel(), -3500)[-3500:]
+        max_ten = origin[partition // 1025, partition % 1103]
+        return max_ten
 
 
 if __name__ == "__main__":
