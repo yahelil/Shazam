b247990d4be9a35c596a4eaf73d7c623efdd3e0e
yahelil <117468179+yahelil@users.noreply.github.com>
2024-02-08 10:55:19 +0200

    Add files via upload

--- Spectrogram.py
+++ Spectrogram.py
@@ -1,84 +1,188 @@
+import math
+import statistics
 import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+from Hash import build_and_add_signature as baas, TracePoint as tp, \
+    DirectionVector as dv, FrequencyMap as fm, find_in_hash as fih
+
+'''
+This file's job is to:
+1. Create a spectrogram for a recording it finds
+2. Find the peaks in the spectrogram
+'''
+
 
 FRAME_SIZE = 2048
-HOP_SIZE = 100
+HOP_SIZE = 1000
 
 
 class Spec:
-    def __init__(self):
-        self.file = "audio/eyal4.wav"
+    def __init__(self, song: str):
+        self.fmap = None
+        self.tp_list = []
+        self.fdhash = None
+        self.file = f"database/{song}.wav"
         self.S_db = None
         self.PeaksSpec = []
+        self.index = []
 
-    def scale(self):
+    def run(self):
+        self.spec()
+        self.peaks()
+        self.hash()
+        song_hash = self.search()
+        self.__init__("sample2")
+        self.spec()
+        self.peaks()
+        self.hash()
+        sample_hash = self.search()
+        print(f"song_hash: {song_hash}\n sample_hash: {sample_hash}")
+        self.match(song_hash, sample_hash)
+
+    def spec(self):
+        """This function presents the spectrogram of the file after the transformation(fft)"""
+
+        # generate spectrogram
         scale, sr = librosa.load(self.file)  # returns audio time series, sampling rate of scale
         S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 
         # Creating a spectrogram out of the audio
         Y_scale = np.abs(S_scale) ** 2
         self.S_db = librosa.amplitude_to_db(Y_scale, ref=np.max)
-        Max, Min = self.max_spec(self.S_db)
+        Max, Min = np.amax(self.S_db), np.amin(self.S_db)
         old_range = [Min, Max]
         new_range = [0, 1]
-        for line in self.S_db:
+        for line in self.S_db:   # loop to create self.PeaksSpec
             self.PeaksSpec.append([self.translate(x, old_range, new_range)for x in line])
-        peaks = self.peaks(self.PeaksSpec)
+        rows, cols = len(self.PeaksSpec), len(self.PeaksSpec[0])
+        for row in range(rows):
+            for col in range(cols):
+                if row < 200:
+                    self.PeaksSpec[row][col] = 0.0
+        # # Just the spectrogram
+        # plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
+        # plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
+        # plt.show()
+
+    def peaks(self):
+        """ In this function all the peaks are presented on top of the spectrogram"""
+
+        # seeking peaks
         rows, cols = len(self.PeaksSpec), len(self.PeaksSpec[0])
         x = []
         y = []
+        averages_up, averages_down = self.find_averages(self.PeaksSpec)
         for row in range(rows):
-            if all(num == 0 for num in self.PeaksSpec[row]):
-                stop = -1 * (len(self.PeaksSpec) - row)
-                self.PeaksSpec = self.PeaksSpec[:stop]
-                break
             for col in range(cols):
-                if self.PeaksSpec[row][col] > peaks[-1]:
+                if self.PeaksSpec[row][col] > averages_up[int(col/256)]: #or 0.1 < self.PeaksSpec[row][col] < averages_down[int(col/256)]
                     self.PeaksSpec[row][col] = 0.5
-                    if row > 3:
+                    if row > 200:
                         x.append(col)
                         y.append(row)
+                        self.index.append((col, row))
                 else:
                     self.PeaksSpec[row][col] = 0.0
-        # Just the spectrogram
-        plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
-        plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
-        plt.show()
-        # The spectrogram with peaks
-        plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
-        plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
-        plt.scatter(x, y, color="red", s=3)
-        plt.show()
+        # # The spectrogram with peaks
+        # plt.imshow(self.PeaksSpec, cmap=plt.get_cmap('gray'))
+        # plt.savefig('original_spectrogram.png', bbox_inches='tight', pad_inches=0)
+        # plt.show()
+
+    def hash(self):
+        """ builds self.tp_list, self.fdhash and self.fmap """
+        x = []
+        y = []
+        for i in range(len(self.index)):
+            freq = self.index[i][1]
+            next_freq = self.index[i-1][1]
+            #print(f"freq: {freq}, next: {next_freq}")
+            if abs(freq - next_freq) >= 1:
+                x.append(self.index[i][0])
+                y.append(freq)
+                #print(f"after:\nfreq: {freq}, next: {next_freq}")
+                length = math.dist(self.index[i], self.index[i-1])
+                TracePoint = tp(freq) if i == 0 else tp(freq, dv(length, math.degrees(math.asin(abs(next_freq-freq)/length))))
+                self.tp_list.append(TracePoint)
+        self.fdhash = baas(self.tp_list)
+        self.fmap = fm(self.fdhash)
+        # plt.scatter(x, y, color="gray", s=5)
+        # plt.show()
+        # print(f"tp_list: {self.tp_list}, fdhash: {self.fdhash}, fmap: {self.fmap}")
+
+    def search(self):
+        """
+        :return: the hash of the song/sample
+        """
+        hash = []
+        for tp in self.tp_list:
+            hash.append(fih(self.fdhash, self.fmap.frequencies, tp))
+        return hash
 
     @staticmethod
-    def max_spec(lst):
-        mymax = -80
-        mymin = 0
-        rows = len(lst)
-        col = len(lst[0])
-        for i in range(rows):
-            for j in range(col):
-                mymax = max(mymax, lst[i][j])
-                mymin = min(mymin, lst[i][j])
-        return mymax, mymin
+    def match(hash1, hash2):
+        '''
+        :param: hash1 - song_hash
+        :param: hash2 - sample_hash
+        :return: Percentage of match
+        '''
+        count = 0
+        total = 0
+        success = 0
+        for data in hash1:
+            if count < len(hash2) - 1:
+                if math.isclose(data[0], hash2[count][0], abs_tol=65) and math.isclose(data[1], hash2[count][1], abs_tol=35):
+                    success += 1
+                if success != 0:
+                    total += 1
+                count += 1
+        print(f"The match percentage is {(success/total)*100}%")
 
     @staticmethod
     def translate(value, src, dest):
+        """
+        :param value: an element in S_db
+        :param src: The range to list used to be
+        :param dest: The range we want the list to be
+        :return: The value in the same ratio but in the new range
+        """
         srcSpan = src[1] - src[0]
         destSpan = dest[1] - dest[0]
         valueScaled = float(value - src[0]) / float(srcSpan)
         return dest[0] + (valueScaled * destSpan)
 
     @staticmethod
-    def peaks(lst):
+    def find_averages(lst):
+        """
+        note - This function is dividing the list into windows and works with each separately
+        :param lst: The list of values in the spectrogram
+        :return: The average between the average of the window to max number and to the min number
+        """
         arr = np.array(lst)
-        origin = arr
-        partition = np.argpartition(origin.ravel(), -3500)[-3500:]
-        max_ten = origin[partition // 1025, partition % 1103]
-        return max_ten
+        max_value, min_value = np.max(arr), np.min(arr)
+        length = len(arr[0])
+        windows = int(length / 256)
+        average_list_up = []
+        average_list_down = []
+        if length % 256 != 0:
+            windows = windows + 1
+        jump = 0
+        next_jump = 0
+        for i in range(windows):
+            next_jump += 256
+            average = np.mean(arr[jump:(length if (windows - 1) else next_jump)])
+            average_up = statistics.mean([average, max_value])
+            average_down = statistics.mean([average, min_value])
+            jump = next_jump
+            average_list_up.append(average_up)
+            average_list_down.append(average_down)
+        return average_list_up, average_down
 
 
 if __name__ == "__main__":
-    Spec().scale()
+    p = Spec("PressStart")
+    p.run()
+    # g = Spec("Symbolism")
+    # g.run()
+
+
