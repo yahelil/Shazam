f18c1f9c2b618384e087d4a54c64774c119ed199
yahelil <117468179+yahelil@users.noreply.github.com>
2024-03-06 12:56:13 +0200

    Add files via upload

--- Spectrogram.py
+++ Spectrogram.py
@@ -20,17 +20,23 @@ HOP_SIZE = 1000
 
 
 class Measures:
-    def __init__(self, peaks_spec):
+    def __init__(self, peaks_spec_list):
         self.AVERAGES_UP = []
         self.AVERAGES_DOWN = []
         self.index = []
-        self.peaks_spec = peaks_spec
+        self.peaks_spec_list = peaks_spec_list
         self.old_range = None
         self.new_range = None
 
-    def generateNormalizeTransformParams(self, s_db):
-        Max, Min = np.amax(s_db), np.amin(s_db)
-        self.old_range = [Min, Max]
+    def generateNormalizeTransformParams(self, s_db_list):
+        max_max, min_min = 0, 0
+        for s_db in s_db_list:
+            Max, Min = np.amax(s_db), np.amin(s_db)
+            if Max > max_max:
+                max_max = Max
+            if Min < min_min:
+                min_min = Min
+        self.old_range = [min_min, max_max]
         self.new_range = [0, 1]
 
     def save_averages(self):
@@ -40,26 +46,33 @@ class Measures:
         :saves: saves The average between average of the window to max num min num to AVERAGES_UP and DOWN
         """
 
-        arr = np.array(self.peaks_spec)
-        max_value, min_value = np.max(arr), np.min(arr)
-        length = len(arr[0])
-        windows = int(length / 256)
-        average_list_up = []
-        average_list_down = []
-        if length % 256 != 0:
-            windows = windows + 1
-        jump = 0
-        next_jump = 0
-        for i in range(windows):
-            next_jump += 256
-            average = np.mean(arr[jump:(length if (windows - 1) else next_jump)])
-            average_up = statistics.mean([average, max_value])
-            average_down = statistics.mean([average, min_value])
-            jump = next_jump
-            average_list_up.append(average_up)
-            average_list_down.append(average_down)
-        self.AVERAGES_UP = average_list_up
-        self.AVERAGES_DOWN = average_list_down
+        # arr = np.array(self.peaks_spec)
+        # max_value, min_value = np.max(arr), np.min(arr)
+        # length = len(arr[0])
+        # windows = int(length / 256)
+        # average_list_up = []
+        # average_list_down = []
+        # if length % 256 != 0:
+        #     windows = windows + 1
+        # jump = 0
+        # next_jump = 0
+        # for i in range(windows):
+        #     next_jump += 256
+        #     average = np.mean(arr[jump:(length if (windows - 1) else next_jump)])
+        #     average_up = statistics.mean([average, max_value])
+        #     average_down = statistics.mean([average, min_value])
+        #     jump = next_jump
+        #     average_list_up.append(average_up)
+        #     average_list_down.append(average_down)
+        # self.AVERAGES_UP = average_list_up
+        # self.AVERAGES_DOWN = average_list_down
+        #for peaks_spec in self.peaks_spec_list:
+        for lst in self.peaks_spec_list:
+            arr = np.array(lst)
+            max_value, min_value = np.max(arr), np.min(arr)
+            average = np.average(arr)
+            self.AVERAGES_UP.append(statistics.mean([average, max_value]))
+            self.AVERAGES_DOWN.append(statistics.mean([average, min_value]))
         # return average_list_up, average_list_down
 
     # def peaks(self):
@@ -83,8 +96,8 @@ class Spec:
         self.tp_list = []
         self.FrequencyDirectionHash = None
         self.file = f"{song}.wav"
-        self.S_db = None
-        self.peaks_spec = []
+        self.S_db_list = []
+        self.peaks_spec_list = []
         self.index = []
         self.indices_list = []
         self.best_match = 0
@@ -99,8 +112,8 @@ class Spec:
         require_normalization = False
         if measures is None:
             require_normalization = True
-            measures = Measures(self.peaks_spec)
-            measures.generateNormalizeTransformParams(self.S_db)
+            measures = Measures(self.peaks_spec_list)
+            measures.generateNormalizeTransformParams(self.S_db_list)
 
         self.normalizingSpec(measures.old_range, measures.new_range)
         if require_normalization:
@@ -118,37 +131,52 @@ class Spec:
         self.match(sample_hash, song_hash, other.fmap)
 
     def normalizingSpec(self, old_range, new_range):
-        for line in self.S_db:  # loop to create self.peaks_spec
-            self.peaks_spec.append([self.translate(x, old_range, new_range) for x in line])
-        rows, cols = len(self.peaks_spec), len(self.peaks_spec[0])
-        for row in range(rows):
-            for col in range(cols):
-                if row < 200:
-                    self.peaks_spec[row][col] = 0.0
+        for S_db in self.S_db_list:
+            peaks_spec = []
+            for line in S_db:  # loop to create self.peaks_spec
+                peaks_spec.append([self.translate(x, old_range, new_range) for x in line])
+            rows, cols = len(peaks_spec), len(peaks_spec[0])
+            for row in range(rows):
+                for col in range(cols):
+                    if row < 200:
+                        peaks_spec[row][col] = 0.0
+            self.peaks_spec_list.append(peaks_spec)
 
-    def SpecBeforeNormilizing(self, fname = "S_DB.csv"):
+
+    def SpecBeforeNormilizing(self, fname="S_DB.csv"):
         """This function presents the spectrogram of the file after the transformation(fft)"""
         # global SDB
         # generate spectrogram
         scale, sr = librosa.load(self.file)  # returns audio time series, sampling rate of scale
-        if fname !=  "S_DB.csv":
-            scale = scale[6000:sr*2+6000]
+        sample_length = sr*2
+        sample_div_2 = int(sample_length/2)
+        if fname != "S_DB.csv":
+            #scale_list = [scale[:int(sample_length/2)], scale[int(sample_length/2):2*sample_length]]
+            scale_list = []
+            sample_length = math.floor(len(scale) / (sample_div_2))
+            for x in range(sample_length):
+                scale_list.append(scale[sample_div_2*x:sample_div_2*(x+1)])
         else:
-            scale = scale[:sr*4]
-        S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+            song_length = math.floor(len(scale)/(sample_div_2))  # The length in the number of times the length if the sample
+            scale_list = []
+            for x in range(song_length):
+                scale_list.append(scale[sample_div_2*x:sample_div_2*(x+1)])
+        for scale in scale_list:
+            S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 
-        # Creating a spectrogram out of the audio
-        Y_scale = np.power(np.abs(S_scale), 2)
-        self.S_db = librosa.amplitude_to_db(Y_scale, ref=np.max)
-        file = open(fname, "w")
-        for x in self.S_db:
-            comma = ""
-            for y in x:
-                if comma == "":
-                    file.write("\n")
-                file.write(f"{comma} {y}")
-                comma = ","
-        file.close()
+            # Creating a spectrogram out of the audio
+            Y_scale = np.power(np.abs(S_scale), 2)
+
+            self.S_db_list.append(librosa.amplitude_to_db(Y_scale, ref=np.max))
+        # file = open(fname, "w")
+        # for x in self.S_db:
+        #     comma = ""
+        #     for y in x:
+        #         if comma == "":
+        #             file.write("\n")
+        #         file.write(f"{comma} {y}")
+        #         comma = ","
+        # file.close()
         # if self.name in DatabaseList:
         #     SDB = self.S_db
         # else:
@@ -160,16 +188,16 @@ class Spec:
 
     def peaks(self, measures):
         """ In this function all the peaks are presented on top of the spectrogram"""
-        rows, cols = len(self.peaks_spec), len(self.peaks_spec[0])
-        for col in range(cols):
-            for row in range(rows):
-                if self.peaks_spec[row][col] > measures.AVERAGES_UP[
-                    0]:  # or 0.1 < self.peaks_spec[row][col] < AVERAGES_DOWN[int(col/256)]
-                    self.peaks_spec[row][col] = 0.5
-                    if row > 200:
+        for peaks_spec in self.peaks_spec_list:
+            rows, cols = len(peaks_spec), len(peaks_spec[0])
+            for col in range(cols):
+                for row in range(rows):
+                    if peaks_spec[row][col] > measures.AVERAGES_UP[
+                        0]:  # or 0.1 < self.peaks_spec[row][col] < AVERAGES_DOWN[int(col/256)]
+                        peaks_spec[row][col] = 0.5
                         self.index.append((col, row))
-                else:
-                    self.peaks_spec[row][col] = 0.0
+                    else:
+                        peaks_spec[row][col] = 0.0
         '''if self.name == "PressStart":
             print("gg")
             INDEXSONG = self.index'''
@@ -262,6 +290,7 @@ class Spec:
             if total > 0 and self.best_match < (matches/total) * 100:
                 self.best_match = (matches/total) * 100
                 winner_tp_song = starter
+            print(f"total = {total}, matchws = {matches}")
             print(f"The match percentage is { (matches/total) * 100 }%")
 
         print(f"The best match percentage is { self.best_match }%")
@@ -345,7 +374,7 @@ if __name__ == "__main__":
     p1 = Spec("PressStart")
     measures = p1.run("song.csv")
 
-    p2 = Spec("‏‏PressStartCopy") # "PressStartEdit")
+    p2 = Spec("sample28")
     p2.run("sample.csv", measures)
 
     p2.compare(p1)
